b2424d6c819b687bed20e956b7b5ec080d567e18
Kanpoz <juanjo2006.jjcc@gmail.com>
2025-11-07 06:45:43 -0500

    Implementacion homes
    ...

---
+++ app/src/main/java/com/jjcc/proyectmovil/homeAcudiente.kt
@@ -0,0 +1,15 @@
+package com.jjcc.proyectmovil
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class homeAcudiente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_acudiente)
+
+    }
+}
